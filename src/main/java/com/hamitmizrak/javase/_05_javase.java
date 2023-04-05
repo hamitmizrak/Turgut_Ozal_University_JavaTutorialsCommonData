038898dc48821792919b8161d1ab4a630fd36576
hamitmizrak <hamitmizrak@gmail.com>
2023-04-05 23:56:19 +0300

    JavaSE tutorials -2

--- src/main/java/com/hamitmizrak/javase/_05_javase.java
+++ src/main/java/com/hamitmizrak/javase/_05_javase.java
@@ -66,4 +66,18 @@ public class _05_javase {
         conditional4();
     }
 
+    /*
+    VKI: Kullanıcıdan öncelikle Bayan mı Erkek mi oludğunu öğrenceksiniz ?
+    klavye.nextLine();
+    Kadınlar için ideal kilo formülü:
+    Kadınlar için ideal kilo hesaplama formülü  45,5 + 2,3 x ( (Boy(cm)/2,54) – 60)
+
+    Erkekler için ideal kilo formülü:
+    Erkekler için ideal kilo hesaplama formülü  50 + 2,3 X ( (Boy(cm)/2,54) – 60)
+    */
+
+    /*
+    Kullanıcıdan aldığımız password ile kullanıcadn aldığımız repassword verisi birbirine eşit mi değil mi ?
+   */
+
 }
