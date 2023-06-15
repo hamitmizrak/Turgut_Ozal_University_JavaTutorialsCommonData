5c0cd7b44226d45bca000d7bbc1ced5c16c33bc6
hamitmizrak <hamitmizrak@gmail.com>
2023-06-15 11:17:47 +0300

    Paralel Programalama Final

--- src/main/java/com/hamitmizrak/finalhome/parallelprogramming/ParallelService.java
+++ src/main/java/com/hamitmizrak/finalhome/parallelprogramming/ParallelService.java
@@ -1,10 +1,23 @@
 package com.hamitmizrak.finalhome.parallelprogramming;
 
+import javax.swing.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ Paralel programlama için Java ile uygulama yapılacaktır. İstenen özellikler aşağıda gösterilmiştir.
+        Step-1 ) Yapılacak uygulama kullanıcıda(user) iki tane farklı veri console üzerinden bilgi alınacaktır. Ancak
+ alınacak veriler belli sırada olması gerekmektedir önce birinci thread çalışacak işlem bittikten sonra ikinci
+ thread çalışacaktır. Bunuda Java Paralel programlama özelliğiyle yapılması gerekiyor. Örneğin: 1.thread
+ çalışacak ve sonrasında 2.thread çalışacaktır. (Thread Join özelliğiyle)
+        Step-2 ) Birinci thread Kulanıcı bilgileri console üzerindenn Scanner nesnesi üzerinden alınacak
+ username,password ve email adres bunu file bilgisayarın c:\\io\\turgutozaluniversitesi\\person.txt dosyasına
+ kaydedilecektir.
+        Step-3 ) İkinci thread çalışacak kullanıcının secretInformation(tanımlama) isterseniz database kaydedin
+ isterseniz c:\\io\\turgutozaluniversitesi\\secret.txt  adındaki dosyaya kaydedin.
+ */
 public class ParallelService extends Thread{
     public static final String PERSON_URL = "c:\\io\\turgutozaluniversitesi\\person.txt";
     public static final String SECRET_URL = "c:\\io\\turgutozaluniversitesi\\secret.txt";
@@ -41,6 +54,7 @@ public class ParallelService extends Thread{
         String secretInformation;
         System.out.println("secretInformation giriniz");
         secretInformation = klavye.nextLine();
+        //secretInformation= JOptionPane.showInputDialog("secretInformation giriniz");
         return secretInformation;
     }
 
@@ -71,6 +85,5 @@ public class ParallelService extends Thread{
         parallelService2.start();
         parallelService2.join();
     }
-
 
 }
